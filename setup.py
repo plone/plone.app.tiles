a97a1ec3154a50dee5844439ea811c1a74b88b8d
hvelarde <hector.velarde@gmail.com>
2012-06-22 16:44:23 -0300

    fixed package distribution

--- setup.py
+++ setup.py
@@ -2,15 +2,25 @@ from setuptools import setup, find_packages
 import os
 
 version = '1.0a1'
+long_description = open("README.txt").read() + "\n" + \
+                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "HISTORY.txt")).read()
 
 setup(name='plone.app.tiles',
       version=version,
       description="Plone UI integration for plone.tiles",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=long_description,
       classifiers=[
+        "Development Status :: 3 Alpha",
         "Framework :: Plone",
+        "Framework :: Plone :: 4.2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='plone tiles deco',
