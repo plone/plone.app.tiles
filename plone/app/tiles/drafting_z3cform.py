a4ae93af3ea8e1d7541aeb6b5057df8c655a71dc
Asko Soukka <asko.soukka@iki.fi>
2017-09-04 00:12:07 +0300

    Fix issue where drafting linked images cleared existing non-drafted scales

--- plone/app/tiles/drafting_z3cform.py
+++ plone/app/tiles/drafting_z3cform.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from Acquisition import aq_base
+from DateTime import DateTime
 from plone.app.drafts.interfaces import ICurrentDraftManagement
 from plone.app.drafts.interfaces import IDraftable
 from plone.app.drafts.proxy import DraftProxy
@@ -6,6 +8,7 @@ from plone.app.tiles.drafting import draftingTileDataContext
 from plone.app.tiles.interfaces import ITilesFormLayer
 from plone.behavior.interfaces import IBehaviorAssignable
 from plone.namedfile.scaling import ImageScaling
+from plone.scale.storage import AnnotationStorage
 from plone.tiles.interfaces import ITile
 from plone.z3cform.traversal import FormWidgetTraversal
 from z3c.form.field import FieldWidgets as FieldWidgetsBase
@@ -54,7 +57,18 @@ def afterTraversalDrafting(event):
 
 @implementer(ITraversable, IPublishTraverse)
 class TileDraftingImageScaling(ImageScaling):
+    """Enable drafting when a view using @@images is referred from
+    edit view
+
+    Note: This is ugly, because this overrides the default @@images, and
+    drafted scales must be saved with the current scales to result in working
+    image src URLs.
+
+    Todo: It should be checked, if simply checking for the existence of
+    _layouteditor-query could be enough for triggering drafting mode.
+    """
     def __init__(self, context, request):
+        self._context = context  # Save the original context
         referer = (request.get('HTTP_REFERER') or '').split('?', 1)[0]
         if referer and IDisplayFormDrafting.providedBy(request) and (
             referer.endswith('/edit') or
@@ -68,6 +82,14 @@ class TileDraftingImageScaling(ImageScaling):
             self._drafting = False
         super(TileDraftingImageScaling, self).__init__(context, request)
 
+    def modified(self):
+        # Modification date must be based on the original context or
+        # image scale storage will be cleared because of too recent _p_mtime
+        # of possible DraftProxy.
+        context = aq_base(self._context)
+        date = DateTime(context._p_mtime)
+        return date.millis()
+
     def scale(
         self,
         fieldname=None,
@@ -77,19 +99,48 @@ class TileDraftingImageScaling(ImageScaling):
         direction='thumbnail',
         **parameters
     ):
-        # Save drafted scales separately from other scales. With concurrent
-        # drafts there may still be race condition.
+        def create():
+            return super(TileDraftingImageScaling, self).scale(
+                fieldname=fieldname,
+                scale=scale,
+                height=height,
+                width=width,
+                direction=direction,
+                **parameters
+            )
+
+        view = create()
+        data = getattr(self._context, view.fieldname)
+        draft = getattr(self.context, view.fieldname)
+
+        # 1) When draft has no changes scale the original
+        if data is draft:
+            self._drafting = False
+
+        # 2) For modified drafts, Save drafted scales separately from other
+        # scales. (With concurrent drafts there may still be race condition.)
         if self._drafting:
-            # Currently the only option to differentiate draft cache key
+            # Currently the only option to differentiate draft cache key by
+            # setting the otherwise unused 'result' parameter to its default
+            # value 'None'
             parameters['result'] = None
-        return super(TileDraftingImageScaling, self).scale(
-            fieldname=fieldname,
-            scale=scale,
-            height=height,
-            width=width,
-            direction=direction,
-            **parameters
-        )
+            view = create()
+
+        # 3) If drafted data is newer than drafted scale, renew the scale:
+        if self._drafting and view.data._p_mtime:
+            if draft._p_mtime > view.data._p_mtime:
+                storage = AnnotationStorage(self.context, self.modified)
+                del storage[view.uid]
+                view = create()
+
+        from datetime import datetime
+        print self.context
+        for value in self.context.__annotations__['plone.scale'].values():
+            print value['uid'], datetime.fromtimestamp(
+                value['modified'] / 1000.), value['key'][-2][-1]
+        print '-' * 80
+
+        return view
 
 
 @adapter(ITileDrafting, IDisplayFormDrafting, Interface)
