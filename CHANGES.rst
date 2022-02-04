8e3e66eee53d2a8252c5f3cbc4b7c91858189b46
Maurits van Rees <maurits@vanrees.org>
2022-02-04 09:17:08 +0100

    Bumped version for feature release.
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-3.2.4 (unreleased)
+3.3.0 (unreleased)
 ------------------
 
 Features:
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "3.2.4.dev0"
+version = "3.3.0.dev0"
 long_description = (
     open("README.rst").read()
     + "\n"
